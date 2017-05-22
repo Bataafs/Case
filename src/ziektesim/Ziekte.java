33a0959d95ad28dda914c5d3a549b6de97229dbf
010737 <010737@BL-109-21.osgh.local>
2017-05-22 11:39:37 +0200

    new commit

---
+++ src/ziektesim/Ziekte.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ziektesim;
+
+/**
+ *
+ * @author 010737
+ */
+public class Ziekte {
+    
+}
